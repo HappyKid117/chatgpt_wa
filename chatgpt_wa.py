a492f02049803c3633ae96afe1fb7c673fedd1d1
HappyKid117 <f20190021@goa.bits-pilani.ac.in>
2023-09-18 01:48:06 +0530

    new version

--- chatgpt_wa.py
+++ chatgpt_wa.py
@@ -15,7 +15,12 @@ current_chat_class_name = '_3W2ap'
 search_box_xpath = '//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p'
 chatgpt_response_xpath = "//*[contains(@class, 'markdown prose w-full break-words dark:prose-invert light')]"
 
-prompt = "Reply with greater burstiness and perplexity but keep it short."
+base_prompt = "Reply with greater burstiness and perplexity but keep it short. "
+
+char_names = ["Jack",
+              "Serena",
+              "Stellar West"
+              ]
 
 def remove_emojis(data):
     emoj = re.compile("["
@@ -60,11 +65,11 @@ def get_latest_message():
     latest_message = messages[len(messages)-1].get_attribute("innerText")
     return latest_message
 
-def send_message(string):
-    driver.switch_to.window(driver.window_handles[1])
+def send_message(string, char):
+    driver.switch_to.window(driver.window_handles[2])
     input_box = driver.find_element(By.CLASS_NAME, inp_class_name)
     messages = convert(string)
-    input_box.send_keys("_*.Soupy:*_ ")
+    input_box.send_keys("_*.{}:*_ ".format(char_names[char]))
     newline()
     for i in messages:
         input_box.send_keys(i)
@@ -74,9 +79,9 @@ def send_message(string):
 
 def send_intro():
     input_box = driver.find_element(By.CLASS_NAME, inp_class_name)
-    input_box.send_keys('_*.Soupy:*_')
+    input_box.send_keys('_*.Stellar West*_')
     newline()
-    input_box.send_keys('Hi, If you want me to ignore your message, put "." at the beginning')
+    input_box.send_keys('Two characters {} and {} have joined this chat. Talk to them by calling out their name first'.format(char_names[0], char_names[1]))
     input_box.send_keys(Keys.ENTER)
 
 def convert(lst):
@@ -88,13 +93,13 @@ def read_chatgpt_response(x):
     print(">Response : " + response)
     return response
 
-def get_chatgpt_response(x):
-    driver.switch_to.window(driver.window_handles[0])
+def get_chatgpt_response(x, char):
+    driver.switch_to.window(driver.window_handles[char])
     chatgpt_input_box = driver.find_element("xpath", (chatgpt_input_box_xpath))
-    chatgpt_input_box.send_keys(x+" "+prompt+Keys.ENTER)
-    time.sleep(5)
+    chatgpt_input_box.send_keys(base_prompt+x+Keys.ENTER)
+    time.sleep(3)
     response = read_chatgpt_response(x);
-    send_message(remove_emojis(response))
+    send_message(remove_emojis(response), char)
 
 def functionality(x):
     print(">Latest message = "+x)
@@ -102,31 +107,35 @@ def functionality(x):
         chat_name = x.replace('soupy go to ', '')
         current_chat = get_current_chat()
         if(current_chat==home_chat):
-            send_message("I am going to "+chat_name)
+            send_message("I am going to "+chat_name, 2)
             go_to_chat(chat_name)
             send_intro()
         else:
-            send_message("You can't ask me to go to "+chat_name+" from "+current_chat)
+            send_message("You can't ask me to go to "+chat_name+" from "+current_chat, 2)
         return
 
     elif(x == "go home soupy"):
-        send_message("bye")
+        send_message("bye", 2)
         go_to_chat(home_chat)
         send_intro()
         return
     
-    elif(x.startswith(".")): #("soupy" in x):
+    elif(x.startswith(".")):
         return
     
-    else:
+    elif(x.startswith("jack ")):
         print(x)
-        get_chatgpt_response(x)
+        get_chatgpt_response(x, 0)
+
+    elif(x.startswith("serena ")):
+        print(x)
+        get_chatgpt_response(x, 1)
 
 def initialize():
     chrome_options = Options()
     chrome_options.add_experimental_option("debuggerAddress", "localhost:8989")
     func_driver = webdriver.Chrome(options=chrome_options)
-    func_driver.switch_to.window(func_driver.window_handles[1])
+    func_driver.switch_to.window(func_driver.window_handles[2])
     print(">Keep the tab active till your home chat appears")
     return func_driver
 
